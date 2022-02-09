272b42e0fb4f605698c26f96c708e6505483cc0b
Alexander Matzen <alexander@amatzen.dk>
2022-02-09 11:01:38 +0100

    JavaDocs for IPostEntityProcessingService.java

--- AsteroidsNetbeansModules/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
+++ AsteroidsNetbeansModules/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
@@ -4,10 +4,17 @@ import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
 /**
+ * The interface Post entity processing service.
  *
  * @author jcs
  */
 public interface IPostEntityProcessingService {
 
+    /**
+     * Process.
+     *
+     * @param gameData the game data
+     * @param world    the world
+     */
     void process(GameData gameData, World world);
 }
