c8cf7092da3b8b2c98e45de81bd0b3bceda11215
Alexander Matzen <alexander@amatzen.dk>
2022-02-09 11:00:56 +0100

    Added pre and post conditions

--- AsteroidsNetbeansModules/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
+++ AsteroidsNetbeansModules/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
@@ -13,6 +13,8 @@ public interface IGamePluginService {
      *
      * @param gameData The game context
      * @param world    The world
+     *
+     * @post The plugin has been instantiated and started
      */
     void start(GameData gameData, World world);
 
@@ -21,6 +23,9 @@ public interface IGamePluginService {
      *
      * @param gameData the game data
      * @param world    the world
+     *
+     * @pre The game plugin has been started
+     * @post The game plugin has been stopped
      */
     void stop(GameData gameData, World world);
 }
