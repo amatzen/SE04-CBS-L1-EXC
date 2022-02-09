39b32037bebfb609332aa5bd7eb2d0b52f156b91
Alexander Matzen <alexander@amatzen.dk>
2022-02-09 11:25:12 +0100

    Wrong project

--- AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
+++ AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
@@ -3,7 +3,16 @@ package dk.sdu.mmmi.cbse.common.services;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
+/**
+ * The interface Entity processing service.
+ */
 public interface IEntityProcessingService {
 
+    /**
+     * Process.
+     *
+     * @param gameData the game data
+     * @param world    the world
+     */
     void process(GameData gameData, World world);
 }
--- AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
+++ AsteroidsEntityFramework/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
@@ -3,8 +3,29 @@ package dk.sdu.mmmi.cbse.common.services;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
+/**
+ * The Game Plugin Service Contract.
+ */
 public interface IGamePluginService {
+
+    /**
+     * Starts a game plugin
+     *
+     * @param gameData The game context
+     * @param world    The world
+     *
+     * @post The plugin has been instantiated and started
+     */
     void start(GameData gameData, World world);
 
+    /**
+     * Stops a game plugin
+     *
+     * @param gameData the game data
+     * @param world    the world
+     *
+     * @pre The game plugin has been started
+     * @post The game plugin has been stopped
+     */
     void stop(GameData gameData, World world);
 }
