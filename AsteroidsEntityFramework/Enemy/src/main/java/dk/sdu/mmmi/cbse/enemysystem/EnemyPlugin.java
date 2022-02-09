db8f8f36ef373b8f6c609fb484e2bc068cd039e0
Alexander Matzen <alexander@amatzen.dk>
2022-02-09 11:23:25 +0100

    Created enemyplugin.java

---
+++ AsteroidsEntityFramework/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyPlugin.java
@@ -0,0 +1,17 @@
+package dk.sdu.mmmi.cbse.enemysystem;
+
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+public class EnemyPlugin implements IGamePluginService {
+    @Override
+    public void start(GameData gameData, World world) {
+        // TODO: Implement start
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        // TODO: Implement stop
+    }
+}
