76b94a43dafc1ef6d76ec96452a8f1679d5a200a
Alexander Matzen <alexander@amatzen.dk>
2022-02-09 10:50:12 +0100

    Added basic javadocs for IGamePluginService

--- AsteroidsNetbeansModules/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
+++ AsteroidsNetbeansModules/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IGamePluginService.java
@@ -3,9 +3,24 @@ package dk.sdu.mmmi.cbse.common.services;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
+/**
+ * The Game Plugin Service Contract.
+ */
 public interface IGamePluginService {
 
+    /**
+     * Starts a game plugin
+     *
+     * @param gameData The game context
+     * @param world    The world
+     */
     void start(GameData gameData, World world);
 
+    /**
+     * Stops a game plugin
+     *
+     * @param gameData the game data
+     * @param world    the world
+     */
     void stop(GameData gameData, World world);
 }
