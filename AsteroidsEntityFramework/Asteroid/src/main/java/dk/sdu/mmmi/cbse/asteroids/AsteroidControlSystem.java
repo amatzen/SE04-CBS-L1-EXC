c7d2b02c937b65aa910810e4ef71f7e351092a97
Alexander Matzen <alexander@amatzen.dk>
2022-02-16 11:06:45 +0100

    Begin asteroid construction

--- AsteroidsEntityFramework/Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroids/AsteroidControlSystem.java
+++ AsteroidsEntityFramework/Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroids/AsteroidControlSystem.java
@@ -1,12 +1,16 @@
 package dk.sdu.mmmi.cbse.asteroids;
 
+import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 
 public class AsteroidControlSystem implements IEntityProcessingService {
     @Override
     public void process(GameData gameData, World world) {
-        // TODO: Implement
+        for (Entity a : world.getEntities(Asteroid.class)) {
+
+        }
     }
 }
